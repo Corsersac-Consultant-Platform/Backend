85f7399e6ab5c450dbe5b72eac4f86101cb78b06
Mathias Jave Diaz <u202212721@upc.edu.pe>
2025-01-13 15:27:09 -0500

    chore: changed sln name

--- Solution1/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Solution1/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251d283fea1a06d161654c77600bf9d6a83aa92a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
